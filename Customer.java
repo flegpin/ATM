5f379e70902eb0590aad07fc3af113a37faded27
Mohamed Berte <flegpin@hotmail.fr>
2021-10-02 20:05:50 -0400

    Various bug fixes, can now withdraw or send cash

--- Customer.java
+++ Customer.java
@@ -1,15 +1,60 @@
 public class Customer {
 	
 	String fname, lname;
-	int balance, attempt, id;
+	int balance, attempt, id, pin;
 	
-	public Customer (String fName, String lName, int Balance)
+	public Customer (String fName, String lName, int Balance, int Pin)
 	{
-		fname= "Moh";
-		lname= "Prod";
-		balance= 1000000;
+		fname= fName;
+		lname= lName;
+		balance= Balance;
+		pin=Pin;
 		id=1000;
 		attempt=3;
-		System.out.println("Welcome " +fname+ " "+lname+"\n"+"U currently have "+balance+" in your account");
+		
+		//System.out.println("Welcome " +fname+ " "+lname+"\n"+"U currently have "+balance+" in your account\n");
 	}
+	
+	public void withdraw(int amount)
+	{
+		balance =balance-amount;	 
+		System.out.println("\nAfter withdrawing " + amount + "your current balance is "+ balance );
+	}
+	public int pain ()
+	{
+		return pin;
+	}
+	public void present()
+	{
+		System.out.println("Welcome " +fname+ " "+lname+"\n"+"U currently have "+balance+" in your account\n");	
+	}
+	public String receiv()
+	{
+		return fname;
+	}
+		public void receive(int amount)
+	{	
+		int old=balance;
+		balance =balance+amount;	 
+		System.out.println("\nAfter receiving " + amount +" "+ fname+" your current balance went from "+old+ " to " + balance +"\n\n" );
+	}
+	/* 
+		@Override
+        public boolean equals(Object obj) {
+           if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+			Customer other = (Customer) obj;
+            if (getClass() != obj.getClass())
+                return false;
+            if (pin == obj.pin)
+                return true;
+						
+			if(pin==3)
+				return true;
+			else
+			return false;
+}
+  */
 }
--- Main.java
+++ Main.java
@@ -1,21 +1,122 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 class Main{
+
  public static void main(String[] args) {
 
-	Scanner myObj = new Scanner(System.in);
-    System.out.println("enter a number");
-	int res= myObj.nextInt();
-	for(int i=0; i<10; i++)
-	{System.out.println(res+"*"+i+"="+i*res);}
+	LocalDate date =  LocalDate.now();
+	LocalTime time =  LocalTime.now();
+	
+	System.out.println("\n\n" + date + "    " + time + "\n\n");
+	
+	//Main Method initializes the Customer object with the firs customers
+	Object [][]firstCustomers = {{"franky","vincent",45000,1234},{"prodi","prodac",65000,2345},
+	{"kini","chef",73000,3}};
+	
+	ArrayList<Customer> customers = new ArrayList <Customer>();
+	
+	for (int i=0;i<=2;i++)
+		{	//Semi automatic initialisation with data cast to make sure the data fits
+			Customer temp= new Customer((String) firstCustomers[i][0],(String) firstCustomers[i][1],(Integer)firstCustomers[i][2],(Integer)firstCustomers[i][3]);
+			customers.add(temp);
+		}
+	System.out.println("Welcome to our Automatic Cash distributor \n");
+	
+	Scanner command = new Scanner(System.in);
+	int pursue = 1;
+	while (pursue != 0 )
+	{
+		
+		System.out.println("\nPlease Enter your PIN\n");
+		int select = command.nextInt();
+		//now we have to find the correct object
 
-	Customer prod= new Customer ("a","b",0);
+		//for loop until we reach the end of the Array while comparing each pin
+		int match=-1;
+		for (int i=0;i<=2;i++)
+		{	//Semi automatic initialisation with data cast to make sure the data fits
+						
+			//found  match with the pin
+		if (customers.get(i).pain() == select )
+			do
+			{
+				match=i;
+				customers.get(i).present();
+				System.out.println("What operation would you like to do today ?\n");
+				System.out.println("1- Withdraw Cash\n");
+				System.out.println("2- Send Interac ( requires receivers ID)\n");
+				System.out.println("3- Log out \n");
+				
+				select = command.nextInt();
+				switch (select){
+					case 1:
+					System.out.println("\nHow much would you like to withdraw today?\n");
+					int amount = command.nextInt();
+					customers.get(i).withdraw(amount);
+					
+					break;
+					case 2:
+					System.out.println("\nPlease enter the receivers ID\n");
+				    amount = command.nextInt();
+					//does the receiver exist 
+					//check if the receiver exists then proceed to withdrawal
+							int match2=-1;
+							for (int j=0;j<=2;j++)
+								if (customers.get(j).pain() == amount )
+								{
+									// position of the receiver in j
+									match2=j;
+									
+								}
+								if (match2 ==(-1) )// no receiver found, we can retry the pin or retry another action
+								{
+									System.out.println("\nError no receiver account matching , please retry PIN or press 0 to quit");
+										select = command.nextInt();
+										if (select ==0 )
+											break;
+										else 
+											continue;
+								}
+								else	//there was a match 
+								{
+									//sending cash between the 2 parties
+									String receiver = customers.get(match2).receiv(); // name of the receiver 
+									System.out.println("\nHow much do you want to send to "+ receiver );
+									amount = command.nextInt();
+									customers.get(i).withdraw(amount);
+									customers.get(match2).receive(amount);
+								}
+								
+								
+							//customers.get(i).withdraw(); 
+					
+					break;
+					case 3:
+					System.out.println("\nYou've successfully logged out\n");
+					break;
+					default:
+					System.out.println("\nWrong input\n");
+				}
+			}
+		while(select != 3 );			
+		}
+		//comparison done, if match is still negative, no user was found, error message and please retry the pin;
+		if (match==(-1))
+		{System.out.println("\nError no account found, please retry or press 0 to quit");
+		select = command.nextInt();}
+		if (select ==0 )
+			break;
+		else 
+			continue;
+	}
+	
+	
+    System.out.println("Thanks for using the service");
  }
 }
-/*
-ok so there are customers, each as a name, id, balance, password, 3 attempts
-they can withdraw, or send the cash to another member if they have the id or name 
+/*z
 
-should i make a hashlist to store those, how can i combine that with an object person.
-ok so i have an arrow with these persons and i will create a method that will add create objects for those guys
 */
